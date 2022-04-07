f0ebf667c006f00e4609830bef61a327d803615a
d-giles <dgiles1@hawk.iit.edu>
2022-04-06 17:18:49 -0700

    Comment out the copyright statement

--- phasefold.py
+++ phasefold.py
@@ -1,8 +1,10 @@
+"""
 Copyright (c) 2022, Yifan Tong
 All rights reserved.
 
 This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree. 
+"""
 
 #import and set up everything
 import sys
